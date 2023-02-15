9b7e832ba6d72e0d6d5f8cbb16a32ce882cfd393
profitweb <chezsimo@yandex.ru>
2023-02-16 00:41:35 +0300

    Resolution

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+       Phone moscovit = new Phone();
+       moscovit.receiveCall("Dasha");
+       Phone zvonoka = new Phone("111", "Nokia", 150 );
+       zvonoka.receiveCall("Natalia", "916111111");
+
+       Phone.countryPhone = "Russia";
+
+       System.out.println(moscovit.countryPhone);
+       System.out.println(zvonoka.countryPhone);
     }
 }
